7cb4ae3cb7ca937f1d74572f556f18250479d5f5
fabionascimentodev <fabio.fbn1@gmail.com>
2023-08-12 22:29:44 -0300

    feat: modal/principal.html

--- view/javascript/modal.js
+++ view/javascript/modal.js
@@ -1,128 +1,128 @@
-function acao(){
+function acao() {
     let modal = document.querySelector('.modal')
     modal.style.display = 'block';
 }
-function fechar(){
+function fechar() {
     let modal = document.querySelector('.modal')
     modal.style.display = 'none';
-} 
-function acao1(){
+}
+function acao1() {
     let modal1 = document.querySelector('.modal1')
     modal1.style.display = 'block';
 }
-function fechar1(){
+function fechar1() {
     let modal1 = document.querySelector('.modal1')
     modal1.style.display = 'none';
 }
-function acao2(){
+function acao2() {
     let modal2 = document.querySelector('.modal2')
     modal2.style.display = 'block';
 }
-function fechar2(){
+function fechar2() {
     let modal2 = document.querySelector('.modal2')
     modal2.style.display = 'none';
 }
-function acao3(){
+function acao3() {
     let modalTodosOsFilmes = document.querySelector('.modalTodosOsFilmes')
     modalTodosOsFilmes.style.display = 'block';
 }
-function fechar3(){
+function fechar3() {
     let modalTodosOsFilmes = document.querySelector('.modalTodosOsFilmes')
     modalTodosOsFilmes.style.display = 'none';
 }
-// function acao4(){
-//     let modalTodosOsFilmes1 = document.querySelector('.modalTodosOsFilmes1')
-//     modalTodosOsFilmes1.style.display = 'block';
-// }
-// function fechar4(){
-//     let modalTodosOsFilmes1 = document.querySelector('.modalTodosOsFilmes1')
-//     modalTodosOsFilmes1.style.display = 'none';
-// }
-// function acao5(){
-//     let modalTodosOsFilmes2 = document.querySelector('.modalTodosOsFilmes2')
-//     modalTodosOsFilmes2.style.display = 'block';
-// }
-// function fechar5(){
-//     let modalTodosOsFilmes2 = document.querySelector('.modalTodosOsFilmes2')
-//     modalTodosOsFilmes2.style.display = 'none';
-// }
-// function acao6(){
-//     let modalTodosOsFilmes3 = document.querySelector('.modalTodosOsFilmes3')
-//     modalTodosOsFilmes3.style.display = 'block';
-// }
-// function fechar6(){
-//     let modalTodosOsFilmes3 = document.querySelector('.modalTodosOsFilmes3')
-//     modalTodosOsFilmes3.style.display = 'none';
-// }
-// function acao7(){
-//     let modalTodosOsFilmes4 = document.querySelector('.modalTodosOsFilmes4')
-//     todosOsFilmes4.style.display = 'block';
-// }
-// function fechar7{
-//     let modalTodosOsFilmes4 = document.querySelector('.modalTodosOsFilmes4')
-//     modalTodosOsFilmes4.style.display = 'none';
-// }
-// function acao8(){
-//     let modalTodosOsFilmes5 = document.querySelector('.modalTodosOsFilmes5')
-//     modalTodosOsFilmes5.style.display = 'block';
-// }
-// function fechar8(){
-//     let modalTodosOsFilmes5 = document.querySelector('.modalTodosOsFilmes5')
-//     modalTodosOsFilmes5.style.display = 'none';
-// }
-// function acao9(){
-//     let modalTodosOsFilmes6 = document.querySelector('.modalTodosOsFilmes6')
-//     todosOsFilmes6.style.display = 'block';
-// }
-// function fechar9(){
-//     let modalTodosOsFilmes6 = document.querySelector('.modalTodosOsFilmes6')
-//     modalTodosOsFilmes6.style.display = 'none';
-// }
-// function acao10(){
-//     let modalTodosOsFilmes7 = document.querySelector('.modalTodosOsFilmes7')
-//     modalTodosOsFilmes7.style.display = 'block';
-// }
-// function fechar10(){
-//     let modalTodosOsFilmes7 = document.querySelector('.modalTodosOsFilmes7')
-//     modalTodosOsFilmes7.style.display = 'none';
-// }
-// function acao11(){
-//     let modalTodosOsFilmes8 = document.querySelector('.modalTodosOsFilmes8')
-//     modalTodosOsFilmes8.style.display = 'block';
-// }
-// function fechar11(){
-//     let modalTodosOsFilmes8 = document.querySelector('.modalTodosOsFilmes8')
-//     modalTodosOsFilmes8.style.display = 'none';
-// }
-// function acao12(){
-//     let modalTodosOsFilmes9 = document.querySelector('.modalTodosOsFilmes9')
-//     modalTodosOsFilmes9.style.display = 'block';
-// }
-// function fechar12(){
-//     let modalTodosOsFilmes9 = document.querySelector('.modalTodosOsFilmes9')
-//     modalTodosOsFilmes9.style.display = 'none';
-// }
-// function acao13(){
-//     let modalTodosOsFilmes10 = document.querySelector('.modalTodosOsFilmes10')
-//     modalTodosOsFilmes10.style.display = 'block';
-// }
-// function fechar13(){
-//     let modalTodosOsFilmes10 = document.querySelector('.modalTodosOsFilmes10')
-//     modalTodosOsFilmes10.style.display = 'none';
-// }
-// function acao14(){
-//     let modalTodosOsFilmes11 = document.querySelector('.modalTodosOsFilmes11')
-//     modalTodosOsFilmes11.style.display = 'block';
-// }
-// function fechar14(){
-//     letmodalTodosOsFilmes11 = document.querySelector('.modalTodosOsFilmes11')
-//     modalTodosOsFilmes11.style.display = 'none';
-// }
-// function acao15(){
-//     let modalTodosOsFilmes12 = document.querySelector('.modalTodosOsFilmes12')
-//     modalTodosOsFilmes12.style.display = 'block';
-// }
-// function fechar15(){
-//     let modalTodosOsFilmes12 = document.querySelector('.modalTodosOsFilmes12')
-//     modalTodosOsFilmes12.style.display = 'none';
-// }
+function acao4() {
+    let modalTodosOsFilmes1 = document.querySelector('.modalTodosOsFilmes1')
+    modalTodosOsFilmes1.style.display = 'block';
+}
+function fechar4() {
+    let modalTodosOsFilmes1 = document.querySelector('.modalTodosOsFilmes1')
+    modalTodosOsFilmes1.style.display = 'none';
+}
+function acao5(){
+    let modalTodosOsFilmes2 = document.querySelector('.modalTodosOsFilmes2')
+    modalTodosOsFilmes2.style.display = 'block';
+}
+function fechar5(){
+    let modalTodosOsFilmes2 = document.querySelector('.modalTodosOsFilmes2')
+    modalTodosOsFilmes2.style.display = 'none';
+}
+function acao6(){
+    let modalTodosOsFilmes3 = document.querySelector('.modalTodosOsFilmes3')
+    modalTodosOsFilmes3.style.display = 'block';
+}
+function fechar6(){
+    let modalTodosOsFilmes3 = document.querySelector('.modalTodosOsFilmes3')
+    modalTodosOsFilmes3.style.display = 'none';
+}
+function acao7(){
+    let modalTodosOsFilmes4 = document.querySelector('.modalTodosOsFilmes4')
+    modalTodosOsFilmes4.style.display = 'block';
+}
+function fechar7(){
+    let modalTodosOsFilmes4 = document.querySelector('.modalTodosOsFilmes4')
+    modalTodosOsFilmes4.style.display = 'none';
+}
+function acao8(){
+    let modalTodosOsFilmes5 = document.querySelector('.modalTodosOsFilmes5')
+    modalTodosOsFilmes5.style.display = 'block';
+}
+function fechar8(){
+    let modalTodosOsFilmes5 = document.querySelector('.modalTodosOsFilmes5')
+    modalTodosOsFilmes5.style.display = 'none';
+}
+function acao9(){
+    let modalTodosOsFilmes6 = document.querySelector('.modalTodosOsFilmes6')
+    modalTodosOsFilmes6.style.display = 'block';
+}
+function fechar9(){
+    let modalTodosOsFilmes6 = document.querySelector('.modalTodosOsFilmes6')
+    modalTodosOsFilmes6.style.display = 'none';
+}
+function acao10(){
+    let modalTodosOsFilmes7 = document.querySelector('.modalTodosOsFilmes7')
+    modalTodosOsFilmes7.style.display = 'block';
+}
+function fechar10(){
+    let modalTodosOsFilmes7 = document.querySelector('.modalTodosOsFilmes7')
+    modalTodosOsFilmes7.style.display = 'none';
+}
+function acao11(){
+    let modalTodosOsFilmes8 = document.querySelector('.modalTodosOsFilmes8')
+    modalTodosOsFilmes8.style.display = 'block';
+}
+function fechar11(){
+    let modalTodosOsFilmes8 = document.querySelector('.modalTodosOsFilmes8')
+    modalTodosOsFilmes8.style.display = 'none';
+}
+function acao12(){
+    let modalTodosOsFilmes9 = document.querySelector('.modalTodosOsFilmes9')
+    modalTodosOsFilmes9.style.display = 'block';
+}
+function fechar12(){
+    let modalTodosOsFilmes9 = document.querySelector('.modalTodosOsFilmes9')
+    modalTodosOsFilmes9.style.display = 'none';
+}
+function acao13(){
+    let modalTodosOsFilmes10 = document.querySelector('.modalTodosOsFilmes10')
+    modalTodosOsFilmes10.style.display = 'block';
+}
+function fechar13(){
+    let modalTodosOsFilmes10 = document.querySelector('.modalTodosOsFilmes10')
+    modalTodosOsFilmes10.style.display = 'none';
+}
+function acao14(){
+    let modalTodosOsFilmes11 = document.querySelector('.modalTodosOsFilmes11')
+    modalTodosOsFilmes11.style.display = 'block';
+}
+function fechar14(){
+    let modalTodosOsFilmes11 = document.querySelector('.modalTodosOsFilmes11')
+    modalTodosOsFilmes11.style.display = 'none';
+}
+function acao15(){
+    let modalTodosOsFilmes12 = document.querySelector('.modalTodosOsFilmes12')
+    modalTodosOsFilmes12.style.display = 'block';
+}
+function fechar15(){
+    let modalTodosOsFilmes12 = document.querySelector('.modalTodosOsFilmes12')
+    modalTodosOsFilmes12.style.display = 'none';
+}
